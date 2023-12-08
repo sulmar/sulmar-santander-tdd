a092d1cd20dec3eb8d36c7cce903e4a04368598e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-12-08 10:02:22 +0100

    Dodanie projektu

---
+++ src/TestApp/Rent.cs
@@ -0,0 +1,19 @@
+﻿namespace TestApp;
+
+public class Rent
+{
+    public User Rentee;
+
+    public bool CanReturn(User user)
+    {
+        if (user == null)
+            throw new ArgumentNullException();
+
+        return user == Rentee || user.IsAdmin;
+    }
+}
+
+public class User
+{
+    public bool IsAdmin;
+}
